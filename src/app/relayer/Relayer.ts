0fec05b30b12e240afa6ed7ff22bd713a3c3fa23
wsels <phaerocode@hotmail.com>
2025-09-29 03:13:19 +0200

    chore: scaffold relayer class

---
+++ src/app/relayer/Relayer.ts
@@ -0,0 +1,15 @@
+import { Wallet } from "ethers";
+
+class Relayer {
+    private relayer: Wallet;
+
+    constructor(_relayer: Wallet) {
+        this.relayer = _relayer;
+    }
+
+    async executeTransaction() {
+
+
+    }
+
+}
